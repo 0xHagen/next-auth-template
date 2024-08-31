d271153867345227cbe5782f4e6a52f9b2db56a4
0xHagen <contact@hagenkanne.com>
2024-08-31 16:56:07 +0200

    feat: login action

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -2,6 +2,7 @@
 
 import * as z from "zod";
 
+import { useState, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
@@ -19,8 +20,13 @@ import { CardWrapper } from "@/components/auth/card-wrapper";
 import { Button } from "@/components/ui/button";
 import { FormError } from "@/components/form-error";
 import { FormSuccess } from "@/components/form-success";
+import { login } from "@/actions/login";
 
 export const LoginForm = () => {
+    const [error, setError] = useState<string | undefined>("");
+    const [success, setSuccess] = useState<string | undefined>("");
+    const [isPending, startTransition] = useTransition();
+
     const form = useForm<z.infer<typeof LoginSchema>>({
         resolver: zodResolver(LoginSchema),
         defaultValues: {
@@ -30,7 +36,16 @@ export const LoginForm = () => {
     });
 
     const onSubmit = (values: z.infer<typeof LoginSchema>) => {
-        console.log(values);
+        setError("")
+        setSuccess("")
+
+        startTransition(() => {
+            login(values)
+                .then((data) => {
+                    setError(data.error)
+                    setSuccess(data.success)
+                })
+        })
     }
 
     return (
@@ -42,7 +57,7 @@ export const LoginForm = () => {
                             <FormItem>
                                 <FormLabel>Email</FormLabel>
                                 <FormControl>
-                                    <Input {...field} placeholder="0xhagen@example.com" type="email"/>
+                                    <Input {...field} disabled={isPending} placeholder="0xhagen@example.com" type="email"/>
                                 </FormControl>
                                 <FormMessage />
                             </FormItem>
@@ -51,15 +66,15 @@ export const LoginForm = () => {
                             <FormItem>
                                 <FormLabel>Password</FormLabel>
                                 <FormControl>
-                                    <Input {...field} placeholder="********" type="password"/>
+                                    <Input {...field} disabled={isPending} placeholder="********" type="password"/>
                                 </FormControl>
                                 <FormMessage />
                             </FormItem>
                         )}/>
                     </div>
-                    <FormError message="" />
-                    <FormSuccess message="" />
-                    <Button type="submit" className="w-full">
+                    <FormError message={error} />
+                    <FormSuccess message={success} />
+                    <Button disabled={isPending} type="submit" className="w-full">
                         Login
                     </Button>
                 </form>
