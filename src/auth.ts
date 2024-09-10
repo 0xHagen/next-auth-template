bf12a32aa351359b97ad7be300a81ca26435094f
0xHagen <contact@hagenkanne.com>
2024-09-10 09:58:39 +0200

    feat: send verification email

--- src/auth.ts
+++ src/auth.ts
@@ -28,6 +28,17 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     }
   },
   callbacks: {
+    async signIn({ user, account}) {
+      if (account?.provider !== "credentials") return true;
+
+      const existingUser = await getUserById(user.id as string);
+
+      if (!existingUser?.emailVerified) return false;
+
+      // TODO: Add 2FA check
+
+      return true;
+    },
     async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
